2937e3f89b1c0bc7f54b13329635f19f37b6bf63
MT0609 <thang123lk@gmail.com>
2020-11-06 21:13:30 +0700

    change pic and modify layout

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,3 +1,9 @@
+const products = require("../models/products.model");
+
 module.exports.get = (req, res) => {
-  res.render("pages/index");
+  const laptops = products.filter((product) => product.type === "laptop");
+  const mobiles = products.filter((product) => product.type === "mobile");
+  const computers = products.filter((product) => product.type === "computer");
+
+  res.render("pages/index", { laptops, mobiles, computers });
 };
--- models/products.model.js
+++ models/products.model.js
@@ -8,7 +8,7 @@ var products = [
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In condimentum quam ac mi viverra dictum. In efficitur ipsum diam, at dignissim lorem tempor in. Vivamus tempor hendrerit finibus. Nulla tristique viverra nisl, sit amet bibendum ante suscipit non. Praesent in faucibus tellus, sed gravida lacus. Vivamus eu diam eros. Aliquam et sapien eget arcu rhoncus scelerisque. Suspendisse sit amet neque neque. Praesent suscipit et magna eu iaculis. Donec arcu libero, commodo ac est a, malesuada finibus dolor. Aenean in ex eu velit semper fermentum. In leo dui, aliquet sit amet eleifend sit amet, varius in turpis. Maecenas fermentum ut ligula at consectetur. Nullam et tortor leo.",
     thumbnail:
-      "https://images-na.ssl-images-amazon.com/images/I/31TLL5nMKZL.jpg",
+      "https://salt.tikicdn.com/cache/w444/ts/product/1f/0d/0a/ade75e655133518889217408afd9c548.jpg",
     thumbnail2:
       "https://www.bajajfinservmarkets.in/emistore/media/catalog/product/j/7/j7prime_gold_additional3.jpg",
     thumbnail3:
@@ -23,7 +23,7 @@ var products = [
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In condimentum quam ac mi viverra dictum. In efficitur ipsum diam, at dignissim lorem tempor in. Vivamus tempor hendrerit finibus. Nulla tristique viverra nisl, sit amet bibendum ante suscipit non. Praesent in faucibus tellus, sed gravida lacus. Vivamus eu diam eros. Aliquam et sapien eget arcu rhoncus scelerisque. Suspendisse sit amet neque neque. Praesent suscipit et magna eu iaculis. Donec arcu libero, commodo ac est a, malesuada finibus dolor. Aenean in ex eu velit semper fermentum. In leo dui, aliquet sit amet eleifend sit amet, varius in turpis. Maecenas fermentum ut ligula at consectetur. Nullam et tortor leo.",
     thumbnail:
-      "https://images-na.ssl-images-amazon.com/images/I/31TLL5nMKZL.jpg",
+      "https://salt.tikicdn.com/cache/w444/ts/product/1f/0d/0a/ade75e655133518889217408afd9c548.jpg",
     thumbnail2:
       "https://www.bajajfinservmarkets.in/emistore/media/catalog/product/j/7/j7prime_gold_additional3.jpg",
     thumbnail3:
@@ -60,6 +60,36 @@ var products = [
       "https://static.quickmobileshop.com/cs-photos/products/original/galaxy-j7-prime-dual-sim-16gb-lte-4g-auriu-3gb-ram_10018509_5_1502267212.jpg",
   },
   {
+    id: "mobile-5",
+    name: "Samsung Galaxy J7 Prime",
+    type: "mobile",
+    basePrice: 280,
+    salePrice: 200,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In condimentum quam ac mi viverra dictum. In efficitur ipsum diam, at dignissim lorem tempor in. Vivamus tempor hendrerit finibus. Nulla tristique viverra nisl, sit amet bibendum ante suscipit non. Praesent in faucibus tellus, sed gravida lacus. Vivamus eu diam eros. Aliquam et sapien eget arcu rhoncus scelerisque. Suspendisse sit amet neque neque. Praesent suscipit et magna eu iaculis. Donec arcu libero, commodo ac est a, malesuada finibus dolor. Aenean in ex eu velit semper fermentum. In leo dui, aliquet sit amet eleifend sit amet, varius in turpis. Maecenas fermentum ut ligula at consectetur. Nullam et tortor leo.",
+    thumbnail:
+      "https://images-na.ssl-images-amazon.com/images/I/31TLL5nMKZL.jpg",
+    thumbnail2:
+      "https://www.bajajfinservmarkets.in/emistore/media/catalog/product/j/7/j7prime_gold_additional3.jpg",
+    thumbnail3:
+      "https://static.quickmobileshop.com/cs-photos/products/original/galaxy-j7-prime-dual-sim-16gb-lte-4g-auriu-3gb-ram_10018509_5_1502267212.jpg",
+  },
+  {
+    id: "mobile-6",
+    name: "Samsung",
+    type: "mobile",
+    basePrice: 280,
+    salePrice: 200,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In condimentum quam ac mi viverra dictum. In efficitur ipsum diam, at dignissim lorem tempor in. Vivamus tempor hendrerit finibus. Nulla tristique viverra nisl, sit amet bibendum ante suscipit non. Praesent in faucibus tellus, sed gravida lacus. Vivamus eu diam eros. Aliquam et sapien eget arcu rhoncus scelerisque. Suspendisse sit amet neque neque. Praesent suscipit et magna eu iaculis. Donec arcu libero, commodo ac est a, malesuada finibus dolor. Aenean in ex eu velit semper fermentum. In leo dui, aliquet sit amet eleifend sit amet, varius in turpis. Maecenas fermentum ut ligula at consectetur. Nullam et tortor leo.",
+    thumbnail:
+      "https://images-na.ssl-images-amazon.com/images/I/31TLL5nMKZL.jpg",
+    thumbnail2:
+      "https://www.bajajfinservmarkets.in/emistore/media/catalog/product/j/7/j7prime_gold_additional3.jpg",
+    thumbnail3:
+      "https://static.quickmobileshop.com/cs-photos/products/original/galaxy-j7-prime-dual-sim-16gb-lte-4g-auriu-3gb-ram_10018509_5_1502267212.jpg",
+  },
+  {
     id: "laptop-1",
     name: "Samsung",
     type: "laptop",
@@ -69,6 +99,66 @@ var products = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In condimentum quam ac mi viverra dictum. In efficitur ipsum diam, at dignissim lorem tempor in. Vivamus tempor hendrerit finibus. Nulla tristique viverra nisl, sit amet bibendum ante suscipit non. Praesent in faucibus tellus, sed gravida lacus. Vivamus eu diam eros. Aliquam et sapien eget arcu rhoncus scelerisque. Suspendisse sit amet neque neque. Praesent suscipit et magna eu iaculis. Donec arcu libero, commodo ac est a, malesuada finibus dolor. Aenean in ex eu velit semper fermentum. In leo dui, aliquet sit amet eleifend sit amet, varius in turpis. Maecenas fermentum ut ligula at consectetur. Nullam et tortor leo.",
     thumbnail:
       "https://product.hstatic.net/1000026716/product/laptop-acer-aspire-3-a315-56-37dv-1_14c2a4369f244da3a18a6a9884da2f0a_2d6425a24d4e40bea850a55c2ff54d59.jpg",
+    thumbnail2:
+      "https://www.bajajfinservmarkets.in/emistore/media/catalog/product/j/7/j7prime_gold_additional3.jpg",
+    thumbnail3:
+      "https://static.quickmobileshop.com/cs-photos/products/original/galaxy-j7-prime-dual-sim-16gb-lte-4g-auriu-3gb-ram_10018509_5_1502267212.jpg",
+  },
+  {
+    id: "laptop-2",
+    name: "Samsung",
+    type: "laptop",
+    basePrice: 280,
+    salePrice: 200,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In condimentum quam ac mi viverra dictum. In efficitur ipsum diam, at dignissim lorem tempor in. Vivamus tempor hendrerit finibus. Nulla tristique viverra nisl, sit amet bibendum ante suscipit non. Praesent in faucibus tellus, sed gravida lacus. Vivamus eu diam eros. Aliquam et sapien eget arcu rhoncus scelerisque. Suspendisse sit amet neque neque. Praesent suscipit et magna eu iaculis. Donec arcu libero, commodo ac est a, malesuada finibus dolor. Aenean in ex eu velit semper fermentum. In leo dui, aliquet sit amet eleifend sit amet, varius in turpis. Maecenas fermentum ut ligula at consectetur. Nullam et tortor leo.",
+    thumbnail:
+      "https://product.hstatic.net/1000026716/product/laptop-acer-aspire-3-a315-56-37dv-1_14c2a4369f244da3a18a6a9884da2f0a_2d6425a24d4e40bea850a55c2ff54d59.jpg",
+    thumbnail2:
+      "https://www.bajajfinservmarkets.in/emistore/media/catalog/product/j/7/j7prime_gold_additional3.jpg",
+    thumbnail3:
+      "https://static.quickmobileshop.com/cs-photos/products/original/galaxy-j7-prime-dual-sim-16gb-lte-4g-auriu-3gb-ram_10018509_5_1502267212.jpg",
+  },
+  {
+    id: "laptop-3",
+    name: "Samsung",
+    type: "laptop",
+    basePrice: 280,
+    salePrice: 200,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In condimentum quam ac mi viverra dictum. In efficitur ipsum diam, at dignissim lorem tempor in. Vivamus tempor hendrerit finibus. Nulla tristique viverra nisl, sit amet bibendum ante suscipit non. Praesent in faucibus tellus, sed gravida lacus. Vivamus eu diam eros. Aliquam et sapien eget arcu rhoncus scelerisque. Suspendisse sit amet neque neque. Praesent suscipit et magna eu iaculis. Donec arcu libero, commodo ac est a, malesuada finibus dolor. Aenean in ex eu velit semper fermentum. In leo dui, aliquet sit amet eleifend sit amet, varius in turpis. Maecenas fermentum ut ligula at consectetur. Nullam et tortor leo.",
+    thumbnail:
+      "https://product.hstatic.net/1000026716/product/laptop-acer-aspire-3-a315-56-37dv-1_14c2a4369f244da3a18a6a9884da2f0a_2d6425a24d4e40bea850a55c2ff54d59.jpg",
+    thumbnail2:
+      "https://www.bajajfinservmarkets.in/emistore/media/catalog/product/j/7/j7prime_gold_additional3.jpg",
+    thumbnail3:
+      "https://static.quickmobileshop.com/cs-photos/products/original/galaxy-j7-prime-dual-sim-16gb-lte-4g-auriu-3gb-ram_10018509_5_1502267212.jpg",
+  },
+  {
+    id: "laptop-4",
+    name: "Samsung",
+    type: "laptop",
+    basePrice: 280,
+    salePrice: 200,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In condimentum quam ac mi viverra dictum. In efficitur ipsum diam, at dignissim lorem tempor in. Vivamus tempor hendrerit finibus. Nulla tristique viverra nisl, sit amet bibendum ante suscipit non. Praesent in faucibus tellus, sed gravida lacus. Vivamus eu diam eros. Aliquam et sapien eget arcu rhoncus scelerisque. Suspendisse sit amet neque neque. Praesent suscipit et magna eu iaculis. Donec arcu libero, commodo ac est a, malesuada finibus dolor. Aenean in ex eu velit semper fermentum. In leo dui, aliquet sit amet eleifend sit amet, varius in turpis. Maecenas fermentum ut ligula at consectetur. Nullam et tortor leo.",
+    thumbnail:
+      "https://product.hstatic.net/1000026716/product/laptop-acer-aspire-3-a315-56-37dv-1_14c2a4369f244da3a18a6a9884da2f0a_2d6425a24d4e40bea850a55c2ff54d59.jpg",
+    thumbnail2:
+      "https://www.bajajfinservmarkets.in/emistore/media/catalog/product/j/7/j7prime_gold_additional3.jpg",
+    thumbnail3:
+      "https://static.quickmobileshop.com/cs-photos/products/original/galaxy-j7-prime-dual-sim-16gb-lte-4g-auriu-3gb-ram_10018509_5_1502267212.jpg",
+  },
+  {
+    id: "laptop-1",
+    name: "Samsung",
+    type: "laptop",
+    basePrice: 280,
+    salePrice: 200,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In condimentum quam ac mi viverra dictum. In efficitur ipsum diam, at dignissim lorem tempor in. Vivamus tempor hendrerit finibus. Nulla tristique viverra nisl, sit amet bibendum ante suscipit non. Praesent in faucibus tellus, sed gravida lacus. Vivamus eu diam eros. Aliquam et sapien eget arcu rhoncus scelerisque. Suspendisse sit amet neque neque. Praesent suscipit et magna eu iaculis. Donec arcu libero, commodo ac est a, malesuada finibus dolor. Aenean in ex eu velit semper fermentum. In leo dui, aliquet sit amet eleifend sit amet, varius in turpis. Maecenas fermentum ut ligula at consectetur. Nullam et tortor leo.",
+    thumbnail:
+      "https://images-na.ssl-images-amazon.com/images/I/31TLL5nMKZL.jpg",
     thumbnail2:
       "https://www.bajajfinservmarkets.in/emistore/media/catalog/product/j/7/j7prime_gold_additional3.jpg",
     thumbnail3:
